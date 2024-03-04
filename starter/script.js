0968914fb15bfdd2372c1f682b4aad5bad777a26
DrErvin <ervin.dragovic@gmail.com>
2024-03-04 23:17:25 +0100

    Update 4code Challange

--- starter/script.js
+++ starter/script.js
@@ -109,6 +109,7 @@ Test data:
 § Data Bonus 2: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 106
 GOOD LUCK �
 */
+/*
 const dolphinsFirstTake = 97;
 const dolphinsSecondTake = 112;
 const dolphinsThirdTake = 101;
@@ -131,6 +132,33 @@ else if (koalasAvg >= 100) {
 else {
     console.log(`No teams reached more than 100 points!`);
 }
-//hej ovo je git commitano
-console.log("ovo je git commitano");
-console.log("Ovo je duplo commitano sa lapitopa");
+*/
+//const countryPop = 35;
+
+//countryPop > 33 ? console.log(`Country pop is above average`) : console.log(`Country pop is below average`);
+/* 
+Coding Challenge #4 
+Steven wants to build a very simple tip calculator for whenever he goes eating 
+in a restaurant. In his country, it's usual to tip 15% if the bill value is 
+between 50 and 300. If the value is different, the tip is 20%. 
+Your tasks: 
+1. Calculate the tip, depending on the bill value. Create a variable called 'tip' for 
+this. It's not allowed to use an if/else statement 
+�
+� (If it's easier for you, you can 
+start with an if/else statement, and then try to convert it to a ternary 
+operator!) 
+2. Print a string to the console containing the bill value, the tip, and the final value 
+(bill + tip). Example: “The bill was 275, the tip was 41.25, and the total value 
+316.25” 
+Test data: 
+§ Data 1: Test for bill values 275, 40 and 430 
+Hints: 
+§ To calculate 20% of a value, simply multiply it by 20/100 = 0.2 
+§ Value X is between 50 and 300, if it's >= 50 && <= 300 
+GOOD LUCK 
+�
+*/
+let tip;
+const bill = 275;
+bill >= 50 && bill <= 300 ? console.log(``):null;
